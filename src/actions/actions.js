7a4d19c2d8d2e1bf3b456768389f13f7e70e5b3a
Edwin Marshall <edwin.marshall@theironyard.com>
2017-08-03 17:03:07 -0400

    With arrows.

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -9,21 +9,11 @@ const setPosts = ({posts}) => ({
 });
 
 const URL = "https://tiny-lasagna-server.herokuapp.com/collections/blogger/";
-
-export function getPosts () {
-    return async function(dispatch) {
+export const getPosts = () => {
+    return async dispatch => {
       dispatch(loading());
-
-      /* with async/await
-      const response = await fetch(URL)
-      const posts = await response.json();
+      const posts = await fetch(URL).then(response => response.json());
       dispatch(setPosts({posts}));
       dispatch(loading(false));
-      */
-
-      fetch(URL).then(response => response.json).then(posts {
-        dispatch(setPosts({posts}));
-        dispatch(loading(false));
-      });
     };
 }
